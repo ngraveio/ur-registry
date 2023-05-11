a45f3a48d0f765314725ce3a75f46f6d0aff6bc6
irfan798 <irfan@contentwise.info>
2023-05-11 16:41:13 +0300

    chore(root): add jest settings to root

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testTimeout: 20000,
+  //testEnvironment: 'node',
+  testPathIgnorePatterns: ['.*/node_modules/'],
+  modulePathIgnorePatterns: ['./dist'],
+  //setupFiles: ['dotenv/config'],
+  //coverageReporters: ["cobertura", "clover", "json", "lcov", "text"],
+  //coveragePathIgnorePatterns: ['.*/node_modules/'],
+};
