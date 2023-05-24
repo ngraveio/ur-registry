0cbf29e7a136d5dc5cea8acdea7efcfb6df3bb04
irfan798 <irfan@contentwise.info>
2023-05-24 16:24:31 +0300

    feat(sign): init package

---
+++ ur-packages/sign-request-response/src/RegistryType.ts
@@ -0,0 +1,6 @@
+import { RegistryType } from "@keystonehq/bc-ur-registry";
+
+export const ExtendedRegistryTypes = {
+  CRYPTO_SIGNATURE_REQUEST: new RegistryType("crypto-sign-request", 1411),
+  CRYPTO_SIGNATURE: new RegistryType("crypto-signature", 1412),
+};
