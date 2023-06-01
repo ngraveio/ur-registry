9002a4b862571791d3ced8c9e8347eed1bf340c4
irfan798 <irfan@contentwise.info>
2023-06-01 15:17:03 +0300

    feat(sign): add sol and xtz metada to map

--- ur-packages/sign-request-response/src/metadatas/index.ts
+++ ur-packages/sign-request-response/src/metadatas/index.ts
@@ -1,12 +1,19 @@
 import { CryptoCoinIdentity, EllipticCurve } from "@ngraveio/bc-ur-registry-crypto-coin-identity";
 import { SignRequestMeta } from '../SignRequestMetadata';
 import { EthSignRequestMeta, PolygonMeta } from "./Ethereum.metadata";
+import { SolSignRequestMeta } from "./solana.metadata";
+import { TezosSignRequestMeta } from "./tezos.metadata";
 
 
 //export const signMetaMap = new Map<CryptoCoinIdentity, typeof SignRequestMeta>([
 export const signMetaMap = new Map<string, typeof SignRequestMeta>([
     [EthSignRequestMeta.coinId.toURL(), EthSignRequestMeta],
     [PolygonMeta.coinId.toURL(), PolygonMeta],
+    [SolSignRequestMeta.coinId.toURL(), SolSignRequestMeta],
+    // Add 3 tezos metadata
+    ['bc-coin://Ed25519/1729', TezosSignRequestMeta],
+    ['bc-coin://secp256k1/1729', TezosSignRequestMeta],
+    ['bc-coin://P256/1729', TezosSignRequestMeta],
 ]);
 
 export function addMetadata(coinId: CryptoCoinIdentity | string, metadata: typeof SignRequestMeta) {
@@ -14,4 +21,6 @@ export function addMetadata(coinId: CryptoCoinIdentity | string, metadata: typeo
     else signMetaMap.set(coinId, metadata);
 }
 
-export { EthSignRequestMeta, PolygonMeta, IrfanSignRequestMeta } from "./Ethereum.metadata";
+export * from './Ethereum.metadata'
+export * from './solana.metadata'
+export * from './tezos.metadata'
