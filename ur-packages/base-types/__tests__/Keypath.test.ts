eea16cf76aa3564925b74f2ba7839cd13a69c539
irfan798 <irfanbilaloglu@gmail.com>
2025-01-22 14:03:14 +0300

    tests: keypath update tests

--- ur-packages/base-types/__tests__/Keypath.test.ts
+++ ur-packages/base-types/__tests__/Keypath.test.ts
@@ -2,6 +2,8 @@ import { Keypath } from "../src/index";
 import { PathComponent } from "../src/classes/PathComponent";
 import { Ur } from "@ngraveio/bc-ur";
 
+// TODO: add more test cases
+
 describe("Keypath", () => {
   it("should create an instance with a single index", () => {
     const keypath = new Keypath({ path: "44'/0'/0'/0/0" });
@@ -9,8 +11,8 @@ describe("Keypath", () => {
   });
 
   it("should create an instance with a range", () => {
-    const keypath = new Keypath({ path: "1h-6h" });
-    expect(keypath.toString()).toBe("1h-6h");
+    const keypath = new Keypath({ path: "1-6h" });
+    expect(keypath.toString()).toBe("1-6'");
   });
 
   it("should create an instance with a wildcard", () => {
@@ -20,20 +22,16 @@ describe("Keypath", () => {
 
   it("should create an instance with a pair", () => {
     const keypath = new Keypath({ path: "<0h;1h>" });
-    expect(keypath.toString()).toBe("<0h;1h>");
+    expect(keypath.toString()).toBe("<0';1'>");
   });
 
   it("should create an instance with mixed components", () => {
     const keypath = new Keypath({ path: "44'/0'/1'-5'/<0h;1h>/*" });
-    expect(keypath.toString()).toBe("44'/0'/1h-5h/<0h;1h>/*");
+    expect(keypath.toString('h')).toBe("44h/0h/1-5h/<0h;1h>/*");
   });
 
   it("should throw an error for invalid range", () => {
-    expect(() => new Keypath({ path: "1-6h" })).toThrow();
-  });
-
-  it("should throw an error for mixed hardening in pair", () => {
-    expect(() => new Keypath({ path: "<0;1h>" })).toThrow();
+    expect(() => new Keypath({ path: "15-7" })).toThrow();
   });
 
   it("should convert to and from UR", () => {
@@ -67,47 +65,47 @@ describe("Keypath", () => {
     expect(() => new Keypath({ path: [], sourceFingerprint: undefined })).toThrow();
   });
 
-  it("should encode to UR and get hex payload for single index", () => {
+  it("should encode to UR and get hex payload for regular path", () => {
     const keypath = new Keypath({ path: "44'/0'/0'/0/0" });
     const ur = keypath.toUr();
     const hex = ur.getPayloadHex();
-    expect(ur.toString()).toBe("ur:keypath/oyadlplfcsdwyklfaeyklfaeyklfaewklfaewkolyapdad"); 
-    expect(hex).toBe("a1018582182cf58200f58200f58200f48200f4"); // {1: [[44, true], [0, true], [0, true], [0, false], [0, false]]}
+    expect(ur.toString()).toBe("ur:keypath/oyadlecsdwykaeykaeykaewkaewkkolbenti"); 
+    expect(hex).toBe("a1018a182cf500f500f500f400f4");
   });
 
   it("should encode to UR and get hex payload for range", () => {
-    const keypath = new Keypath({ path: "1h-6h" });
+    const keypath = new Keypath({ path: "1-6h" });
     const ur = keypath.toUr();
     const hex = ur.getPayloadHex();
-    expect(ur.toString()).toBe("ur:keypath/oyadlylflfadamykiyiooetd");
-    expect(hex).toBe("a1018182820106f5"); // {1: [[[1, 6], true]]}
+    expect(ur.toString()).toBe("ur:keypath/oyadlflfadamykoytllfhs");
+    expect(hex).toBe("a10182820106f5");
   });
 
   it("should encode to UR and get hex payload for wildcard", () => {
     const keypath = new Keypath({ path: "44'/0'/0'/*" });
     const ur = keypath.toUr();
     const hex = ur.getPayloadHex();
-    expect(ur.toString()).toBe("ur:keypath/oyadlrlfcsdwyklfaeyklfaeyklflawkiheoptnn");
-    expect(hex).toBe("a1018482182cf58200f58200f58280f4"); // {1: [[44, true], [0, true], [0, true], [[], false]]}
+    expect(ur.toString()).toBe("ur:keypath/oyadlocsdwykaeykaeyklawkmetlwdkg");
+    expect(hex).toBe("a10188182cf500f500f580f4");
   });
 
   it("should encode to UR and get hex payload for pair", () => {
     const keypath = new Keypath({ path: "<0h;1h>" });
     const ur = keypath.toUr();
     const hex = ur.getPayloadHex();
-    expect(ur.toString()).toBe("ur:keypath/oyadlylflfaeyklfadykvatospfh");
-    expect(hex).toBe("a10181828200f58201f5"); // {1: [[[0, true], [1, true]]]}
+    expect(ur.toString()).toBe("ur:keypath/oyadlylraeykadykguzoieas");
+    expect(hex).toBe("a101818400f501f5");
   });
 
   it("should encode to UR and get hex payload for mixed components", () => {
     const keypath = new Keypath({ path: "44'/0'/1'-5'/<0h;1h>/*" });
     const ur = keypath.toUr();
     const hex = ur.getPayloadHex();
-    expect(ur.toString()).toBe("ur:keypath/oyadlplfcsdwyklfaeyklflfadahyklflfaeyklfadyklflawkbykiaszc"); // Replace with actual expected UR string
-    expect(hex).toBe("a1018582182cf58200f582820105f5828200f58201f58280f4"); // {1: [[44, true], [0, true], [[1, 5], true], [[0, true], [1, true]], [[], false]]}
+    expect(ur.toString()).toBe("ur:keypath/oyadldcsdwykaeyklfadahyklraeykadyklawkosrfgmct");
+    expect(hex).toBe("a10189182cf500f5820105f58400f501f580f4");
   });
 
-  describe.only("Zero common test verctor", () => {
+  describe("Zero common test verctor", () => {
 
   /**
    * @brief Test the encoding and decoding of this CDDL example
@@ -118,8 +116,8 @@ describe("Keypath", () => {
    * }
    * 
    */
-    it("should encode / decode m/1'/2/3-4/5-6'/*/*'/<7;8'>/<9';0>", ()=> {
-      const path = "m/1'/2/3-4/5-6'/*/*'/<7;8'>/<9';0>";
+    it("should encode / decode 1'/2/3-4/5-6'/*/*'/<7;8'>/<9';0>", ()=> {
+      const path = "1'/2/3-4/5-6'/*/*'/<7;8'>/<9';0>";
       const expectedCBOR = "a3018e01f502f4820304f4820506f580f480f58407f408f58409f500f4021a075bcd150308";
       const expectedUr = "ur:keypath/otadmnadykaowklfaxaawklfahamyklawklayklratwkayyklrasykaewkaocyathpsnbzaxayknjtvwty";
 
@@ -161,21 +159,21 @@ describe("Keypath", () => {
    * 
    */
     it("should encode / decode m/44'/200/50/0-100", ()=> {
-      const path = "m/44'/200/50/0-100";
+      const path = "44'/200/50/0-100";
       const expectedCBOR = "A20188182CF518C8F41832F482001864F40304".toLowerCase();
       const expectedUr = "ur:keypath/oeadlocsdwykcsspwkcseywklfaecsiewkaxaarsonnbkk";
 
       // Encoding
       const myPath = new Keypath({ path });
+      myPath.setDepth();
       const ur = myPath.toUr();
       const hex = ur.getPayloadHex();
 
-      expect(myPath.data.sourceFingerprint).toBe(123456789);
-      expect(myPath.data.depth).toBe(8);
+      expect(myPath.data.depth).toBe(4);
       expect(myPath.toString()).toBe(path);
 
-      expect(ur.toString()).toBe(expectedUr);
       expect(hex).toBe(expectedCBOR);
+      expect(ur.toString()).toBe(expectedUr);
 
       // Decoding UR
       const decodedUr = Ur.fromString(expectedUr);
@@ -200,8 +198,8 @@ describe("Keypath", () => {
      * }
      * 
     */
-    it("should encode / decode m/<44';44'>/76500/0/*", ()=> {
-      const path = "m/<44';44'>/76500/0/*";
+    it("should encode / decode <44';44'>/76500/0/*", ()=> {
+      const path = "<44';44'>/76500/0/*";
       const expectedCBOR = "A2018784182CF5182CF51A00012AD4F400F480F40201".toLowerCase();
       const expectedUr = "ur:keypath/oeadltlrcsdwykcsdwykcyaeaddrtywkaewklawkaoadnscsptbk";
 
@@ -210,12 +208,12 @@ describe("Keypath", () => {
       const ur = myPath.toUr();
       const hex = ur.getPayloadHex();
 
-      expect(myPath.data.sourceFingerprint).toBe(123456789);
-      expect(myPath.data.depth).toBe(8);
+      expect(myPath.data.sourceFingerprint).toBe(1);
+      expect(myPath.data.depth).toBe(undefined);
       expect(myPath.toString()).toBe(path);
 
-      expect(ur.toString()).toBe(expectedUr);
       expect(hex).toBe(expectedCBOR);
+      expect(ur.toString()).toBe(expectedUr);
 
       // Decoding UR
       const decodedUr = Ur.fromString(expectedUr);
@@ -240,21 +238,20 @@ describe("Keypath", () => {
      * 
      */
     it("should encode / decode empty path", ()=> {
-      const path = "m";
       const expectedCBOR = "A30180021A3661565D0300".toLowerCase();
       const expectedUr = "ur:keypath/otadlaaocyenhshfhlaxaeuohtswft";
 
       // Encoding
-      const myPath = new Keypath({ sourceFingerprint:912348765, path });
+      const myPath = new Keypath({ sourceFingerprint:912348765 });
+      myPath.setDepth();
       const ur = myPath.toUr();
       const hex = ur.getPayloadHex();
 
-      expect(myPath.data.sourceFingerprint).toBe(123456789);
-      expect(myPath.data.depth).toBe(8);
-      expect(myPath.toString()).toBe(path);
+      expect(myPath.data.sourceFingerprint).toBe(912348765);
+      expect(myPath.data.depth).toBe(0);
 
-      expect(ur.toString()).toBe(expectedUr);
       expect(hex).toBe(expectedCBOR);
+      expect(ur.toString()).toBe(expectedUr);
 
       // Decoding UR
       const decodedUr = Ur.fromString(expectedUr);
@@ -266,7 +263,6 @@ describe("Keypath", () => {
       // Decoding item      
       const decodedKeypath = decodedUr.decode() as Keypath;
       expect(decodedKeypath instanceof Keypath).toBe(true);
-      expect(decodedKeypath.toString()).toBe(path);
     });
 
     /**
@@ -276,8 +272,8 @@ describe("Keypath", () => {
      * }
      * 
      */
-    it("should encode / decode m/<64;64'>", ()=> {
-      const path = "m/<64;64'>";
+    it("should encode / decode <64;64'>", ()=> {
+      const path = "<64;64'>";
       const expectedCBOR = "A10181841840F41840F5".toLowerCase();
       const expectedUr = "ur:keypath/oyadlylrcsfzwkcsfzykrtgeqzjt";
 
@@ -286,12 +282,10 @@ describe("Keypath", () => {
       const ur = myPath.toUr();
       const hex = ur.getPayloadHex();
 
-      expect(myPath.data.sourceFingerprint).toBe(123456789);
-      expect(myPath.data.depth).toBe(8);
       expect(myPath.toString()).toBe(path);
 
-      expect(ur.toString()).toBe(expectedUr);
       expect(hex).toBe(expectedCBOR);
+      expect(ur.toString()).toBe(expectedUr);
 
       // Decoding UR
       const decodedUr = Ur.fromString(expectedUr);
@@ -314,13 +308,13 @@ describe("Keypath", () => {
      * 
      */
     it("should encode and decode from KeypathComponents", () => {
-      const path = "m/98'/2-6/*'/<78200';0'>";
-      const expectedCBOR = "A10181841840F41840F5".toLowerCase();
-      const expectedUr = "ur:keypath/oyadlylrcsfzwkcsfzykrtgeqzjt";
+      const path = "98'/2-6/*'/<78200';0'>";
+      const expectedCBOR = "A101871862F5820206F480F5841A00013178F500F5".toLowerCase();
+      const expectedUr = "ur:keypath/oyadltcsidyklfaoamwklayklrcyaeadehksykaeyknlbtuomn";
 
       const comp1 = new PathComponent({ index: 98, hardened: true });
       const comp2 = new PathComponent({ range: [2, 6] });
-      const comp3 = new PathComponent({ wildcard: true });
+      const comp3 = new PathComponent({ wildcard: true, hardened: true });
       const comp4 = new PathComponent({ pair: [{ index: 78200, hardened: true }, { index: 0, hardened: true } ] });
 
       // Encoding
@@ -328,12 +322,10 @@ describe("Keypath", () => {
       const ur = myPath.toUr();
       const hex = ur.getPayloadHex();
 
-      expect(myPath.data.sourceFingerprint).toBe(123456789);
-      expect(myPath.data.depth).toBe(8);
       expect(myPath.toString()).toBe(path);
 
-      expect(ur.toString()).toBe(expectedUr);
       expect(hex).toBe(expectedCBOR);
+      expect(ur.toString()).toBe(expectedUr);
 
       // Decoding UR
       const decodedUr = Ur.fromString(expectedUr);
@@ -350,34 +342,28 @@ describe("Keypath", () => {
 
   });
 
-  describe("Keypath Error Handling", () => {
-    it("should return an error when the derivation path is without the 'm' root", () => {
-      expect(() => new Keypath({ path: "/76/0'/4" })).toThrow();
-    });
-  
+  describe("Keypath Error Handling", () => {  
     it("should return an error when the derivation path does not match any component regex", () => {
-      expect(() => new Keypath({ path: "m/6/7432'/1nc0rr3ct" })).toThrow();
+      expect(() => new Keypath({ path: "6/7432'/1nc0rr3ct" })).toThrow();
     });
   
     it("should return an error when the fingerprint is defined as 0", () => {
-      const keypath = new Keypath({ path: "m/45'/0" });
-      keypath.data.sourceFingerprint = 0;
-      expect(() => keypath.toUr()).toThrow();
+      expect(() => new Keypath({ path: "45'/0", sourceFingerprint: 0 })).toThrow();
     });
   
     it("should return an error when the child index is superior or equal to 0x80000000", () => {
-      expect(() => new Keypath({ path: "m/50000'/249000/2147483648" })).toThrow();
+      expect(() => new Keypath({ path: "50000'/249000/2147483648" })).toThrow();
     });
   
     it("should return an error when the low index is superior to the high index", () => {
-      expect(() => new Keypath({ path: "m/78400/89'/9'/[76,34]" })).toThrow();
+      expect(() => new Keypath({ path: "78400/89'/9'/[76,34]" })).toThrow();
     });
   
     it("should decode map index in a random order", () => {
       const urToDecode = "ur:keypath/otaocyaeasztdraxaaadloadykaowkcfaohdykcsfxyklnspbnkn";
       const decodedUr = Ur.fromString(urToDecode);
       const decodedKeypath = decodedUr.decode() as Keypath;
-      expect(decodedKeypath.toString()).toBe("m/1'/2/600'/67'");
+      expect(decodedKeypath.toString()).toBe("1'/2/600'/67'");
       expect(decodedKeypath.getSourceFingerprint()).toBe(654378);
       expect(decodedKeypath.getDepth()).toBe(4);
     });
@@ -409,35 +395,40 @@ describe("Keypath", () => {
   
     it("should return an error due to malformed keypath UR types", () => {
       const urErrors = [
-        "ur:keypath/oyadlyadlatogmts",
-        "ur:keypath/oyadlylacyjsvyhs",
-        "ur:keypath/oyadlylyaewnrpzevl",
-        "ur:keypath/oyadlylscfaolsykashpoxdygm",
-        "ur:keypath/oyadlylfaacfaxlrvabzbnbg",
-        "ur:keypath/oyadlylrcshgcfaxlrykwkcaehaedw",
+        "ur:keypath/oyadlyadlatogmts", // {1: [1]}
+        "ur:keypath/oyadlylacyjsvyhs", // {1: [[]]}
+        "ur:keypath/oyadlylyaewnrpzevl", // {1: [[0]]} 
+        "ur:keypath/oyadlylscfaolsykashpoxdygm", // {1: [[4, 900]]}
+        "ur:keypath/oyadlylfaacfaxlrvabzbnbg", // {1: [[4, 900]]} 
+        "ur:keypath/oyadlylrcshgcfaxlrykwkcaehaedw", // no {1: [[87, 900, true, false]]} // no
         "ur:keypath/oyadlylpatcyaeadhemhykwkcfcpfsspvaghbn",
         "ur:keypath/oyadlnlfadaowkykamcsetwkecfxaode",
-        "ur:keypath/oyadlflfadiahsidiaykvoztbdfp",
-        "ur:keypath/oyadlylraeininjtiajljpjpihiajyatwkstwnjsbe",
+        "ur:keypath/oyadlflfadiahsidiaykvoztbdfp", // no
+        "ur:keypath/oyadlylraeininjtiajljpjpihiajyatwkstwnjsbe", // no
         "ur:keypath/oyadlflaiojeihkkjohsjyisaytasegm",
         "ur:keypath/oeadlflaykaosagscxgwtohyfmdaaohsbeaeaeaebgenmhdr",
-        "ur:keypath/otaocyaomwbnesadlflfatasykaxcfbwdkfppmidmt",
+        "ur:keypath/otaocyaomwbnesadlflfatasykaxcfbwdkfppmidmt", // no
         "ur:keypath/otaoykadlfahykaxasnlcfcpry",
-        "ur:keypath/otaochadlfcfaxuywkaxwkpmjkrtqz",
+        "ur:keypath/otaochadlfcfaxuywkaxwkpmjkrtqz", // no
         "ur:keypath/oyadamtslskneh",
         "ur:keypath/nbaatygsih",
-        "ur:keypath/oeadlfaoykadlfcfadspwklrtosgos",
-        "ur:keypath/otadlfaewkaoadaocfaohdpkmkgubw",
-        "ur:keypath/otadlfaewkaxadaxaaimlaykhh",
+        "ur:keypath/oeadlfaoykadlfcfadspwklrtosgos", // no
+        "ur:keypath/otadlfaewkaoadaocfaohdpkmkgubw", // no
+        "ur:keypath/otadlfaewkaxadaxaaimlaykhh", // no
         "ur:keypath/",
         "ur:keypath/randomstring",
         "ur:keypath/rtotbw",
         "ur:keypath/zmeygehflkihghaeasltjefxdmrkbdnsrt"
       ];
+
   
       urErrors.forEach(urError => {
-        expect(() => Ur.fromString(urError).decode()).toThrow();
+        expect(() => {
+          Ur.fromString(urError).decode();
+      }).toThrow();
       });
+
+      expect(true).toBe(true);
     });
 });
 
