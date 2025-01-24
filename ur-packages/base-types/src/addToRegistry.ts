f724d19260bfe8e68bfbcc025aa9c2d437e3aebe
irfan798 <irfanbilaloglu@gmail.com>
2025-01-24 17:48:58 +0300

    feat: implement Bytes

--- ur-packages/base-types/src/addToRegistry.ts
+++ ur-packages/base-types/src/addToRegistry.ts
@@ -1,11 +1,13 @@
 import { UrRegistry } from '@ngraveio/bc-ur'
+import { Bytes } from './Bytes'
 import { CoinInfo } from './CoinInfo'
 import { PSBT } from './PSBT'
 import { Keypath } from './Keypath'
 import { HDKey } from './HDKey'
 
 // Add Classes to the global registry
+UrRegistry.addItem(Bytes)
 UrRegistry.addItem(CoinInfo)
 UrRegistry.addItem(PSBT)
 UrRegistry.addItem(Keypath)
-UrRegistry.addItem(HDKey);
+UrRegistry.addItem(HDKey)
--- ur-packages/base-types/src/index.ts
+++ ur-packages/base-types/src/index.ts
@@ -1,6 +1,7 @@
 // Add Classes to the global registry
 import './addToRegistry'
 
+export { Bytes } from './Bytes'
 export { CoinInfo, Network } from './CoinInfo'
 export { PSBT } from './PSBT'
 export { Keypath } from './Keypath'
