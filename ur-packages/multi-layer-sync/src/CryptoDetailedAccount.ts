c326bb46bea45503a0e5569b68b9e1d07b2a2026
irfan798 <irfanbilaloglu@gmail.com>
2025-02-10 15:37:15 +0300

    feat(multi-layer): refactor DetailedAccount with bc-ur2

--- ur-packages/multi-layer-sync/src/CryptoDetailedAccount.ts
+++ ur-packages/multi-layer-sync/src/CryptoDetailedAccount.ts
@@ -1,214 +1,183 @@
-import { extend, DataItem, RegistryItem, DataItemMap, CryptoHDKey, CryptoOutput } from '@keystonehq/bc-ur-registry';
-import { ExtendedRegistryTypes } from './RegistryType';
-import { HexString } from '@ngraveio/bc-ur-registry-hex-string';
+import { HDKey, OutputDescriptor } from '@ngraveio/bc-ur-registry'
+import { HexString } from '@ngraveio/bc-ur-registry-hex-string'
 
-const { RegistryTypes, decodeToDataItem } = extend;
+import { registryItemFactory } from '@ngraveio/bc-ur'
 
-/** CDDL
- *
- * account_exp = #6.303(crypto-hdkey) / #6.308(crypto-output)
- *
- * ; Accounts are specified using either '#6.303(crypto-hdkey)' or
- * ; '#6.308(crypto-output)'.
- * ; By default, '#6.303(crypto-hdkey)' should be used to share public keys and
- * ; extended public keys.
- * ; '#6.308(crypto-output)' should be used to share an output descriptor,
- * ; e.g. for multisig.
- *
- * token-id = uint / bytes
- *
- * ; Optional 'token-ids' to indicate the synchronization of a list of tokens with
- * ; the associated accounts
- * ; 'token-id' is defined differently depending on the blockchain:
- * ; - ERC20 tokens on EVM chains are identified by their contract addresses
- * ; (e.g. `0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48`)
- * ; - ERC1155 tokens are identifed with their contract addresses followed by their
- * ; ID with ':' as separator (e.g. `0xfaafdc07907ff5120a76b34b731b278c38d6043c:
- * ; 508851954656174721695133294256171964208`)
- * ; - ESDT tokens on MultiversX are by their name followed by their ID with `-` as
- * ; separator (e.g. `USDC-c76f1f`)
- * ; - SPL tokens on Solana are identified by their contract addresses
- * ; (e.g. `EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v`)
- *
- * detailed-account = {
- *   account: account_exp,
- *   ? token-ids: [+ token-id],  ; Specify multiple tokens associated to one account
- *   * tstr => any               ; Extendable optional parameters
- * }
- *
- * ; The extendable new type should be assumed to be generally ignored by the
- * ; watch-only wallets.
- * ; In the case where a specific offline signer and a specific watch-only wallet
- * ; agree to implement an extendable new type, this one could be used in order
- * ; to synchronize an additional information regarding the shared accounts
- * ; (e.g. the information "Hidden: true" added to a coin could indicate to the
- * ; watch-only wallet not to display to the user the associated accounts).
- *
- * account = 1
- * token-ids = 2
- */
-
-type token_id = string | HexString;
-type token_id_input = string | Buffer | HexString;
-
-//account_exp = #6.303(crypto-hdkey) / #6.308(crypto-output)
-type account_exp = CryptoHDKey | CryptoOutput;
-
-enum Keys {
-  account = 1,
-  tokenIds = 2,
+type account_exp = HDKey | OutputDescriptor
+type tokenId = string | HexString
+interface IDetailAccountInput {
+  account: account_exp
+  tokenIds?: tokenId[] // Specify multiple tokens associated to one account
 }
 
-export class CryptoDetailedAccount extends RegistryItem {
-  private account: account_exp;
-  private tokenIds?: token_id[];
+export class DetailedAccount extends registryItemFactory({
+  tag: 1402,
+  URType: 'detailed-account',
+  keyMap: {
+    account: 1,
+    tokenIds: 2,
+  },
+  allowKeysNotInMap: false,
+  CDDL: `
+    account_exp = #6.40303(hdkey) / #6.40308(output-descriptor)
 
-  getRegistryType = () => ExtendedRegistryTypes.CRYPTO_DETAILED_ACCOUNT;
+    ; Accounts are specified using either '#6.40303(hdkey)' or 
+    ; '#6.40308(output-descriptor)'.
+    ; By default, '#6.40303(hdkey)' should be used to share public keys and
+    ; extended public keys.
+    ; '#6.308(output-descriptor)' should be used to share an output descriptor, 
+    ; e.g. for the different Bitcoin address formats (P2PKH, P2SH-P2WPKH, P2WPKH, P2TR).
 
-  constructor(account: account_exp, tokenIds?: token_id_input[]) {
-    super();
-    // Validate inputs
-    CryptoDetailedAccount.checkAccount(account);
+    ; Optional 'token-ids' to indicate the synchronization of a list of tokens with
+    ; the associated accounts
+    ; 'token-id' is defined differently depending on the blockchain:
+    ; - ERC20 tokens on EVM chains are identified by their contract addresses 
+    ; (e.g. "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")
+    ; - ERC1155 tokens are identifed with their contract addresses followed by their 
+    ; ID with ':' as separator (e.g. "0xfaafdc07907ff5120a76b34b731b278c38d6043c:
+    ; 508851954656174721695133294256171964208")
+    ; - ESDT tokens on MultiversX are by their name followed by their ID with "-" as 
+    ; separator (e.g. "USDC-c76f1f")
+    ; - SPL tokens on Solana are identified by their contract addresses
+    ; (e.g. "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v")
 
-    this.account = account;
+    detailed-account = { 
+      account: account_exp,
+      ? token-ids: [+ string / bytes] ; Specify multiple tokens associated to one account
+    }
+
+    account = 1
+    token-ids = 2  
+  `,
+}) {
+  data: IDetailAccountInput
+
+  constructor(data: IDetailAccountInput) {
     // Token Ids are just hex string for erc20 token like 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
     // For ESDT and SPL types its going to be encoded as string
     // We will try to parse the string into bytes with removing 0x part,
     // if we cannot we will encode it as utf8 string
-    if (Array.isArray(tokenIds)) {
-      this.tokenIds = tokenIds.map((tokenId) => {
-        // If its an instance of hexString already just return it
-        if (tokenId instanceof HexString) return tokenId;
+    super(data)
+    this.data = data
 
+    let tokenIds = data.tokenIds
+
+    // If token ids are provided, convert them to hex strings
+    if (tokenIds !== undefined && Array.isArray(data.tokenIds)) {
+      // Try to parse them into hex strings
+      tokenIds = tokenIds.map(tokenId => {
+        // If its already hexstring, return it
+        if (tokenId instanceof HexString) return tokenId as HexString;
         // If its buffer or a string try to parse as HexString
         try {
-          return new HexString(tokenId);
+          return new HexString(tokenId)
         } catch (error) {
-          // TODO: check the error if its not hex
-          return tokenId as string;
+          /* no-op */
+          return tokenId as string
         }
-      });
+      })
     }
+  }
+
+  override verifyInput(input: IDetailAccountInput): { valid: boolean; reasons?: Error[] } {
+    const errors: Error[] = []
+
+    // Checks for the accounts
+    if (!input.account) {
+      errors.push(new Error('Account must be provided'))
+    } else {
+      try {
+        DetailedAccount.checkAccount(input.account)
+      } catch (error) {
+        errors.push(error as Error)
+      }
+    }
+
+    // Checks for the token ids
+    if (input.tokenIds !== undefined) {
+      // Token ids must be an array
+      if (!Array.isArray(input.tokenIds)) {
+        errors.push(new Error('Token ids must be an array'))
+      } else {
+        // Token ids must be either string or uint8array (Buffer is also Uint8Array)
+        input.tokenIds.forEach(tokenId => {
+          if (typeof tokenId !== 'string' && !(tokenId instanceof Uint8Array) && !(tokenId instanceof HexString)) {
+            errors.push(new Error('Token id must be either a string or a Uint8Array or HexString'))
+          }
+        })
+      }
+    }
+
+    return {
+      valid: errors.length === 0,
+      reasons: errors.length > 0 ? errors : undefined,
+    }
+  }
+
+  public getAccount = () => this.data.account
+  public getHdKey = () => {
+    return this.data.account instanceof HDKey ? this.data.account : undefined
+  }
+  public getOutputDescriptor = () => {
+    return this.data.account instanceof OutputDescriptor ? this.data.account : undefined
+  }
+  public getTokenIds = () => {
+    if (!this.data.tokenIds) return undefined
+
+    // Always return string representation of the token ids
+    return this.data.tokenIds.map((tokenId: HexString | string) => {
+      if (tokenId instanceof HexString) {
+        // Shall we really add 0x to start?
+        return '0x' + (tokenId as HexString).getData()
+      }
+      return tokenId as string
+    })
   }
 
   static checkAccount(account: account_exp) {
-    if (!(account instanceof CryptoHDKey) && !(account instanceof CryptoOutput)) {
-      throw new Error('account must be instance of CryptoHDKey or CryptoOutput');
+    // If account is HDKey, check if it has origin and a valid path
+    if (account instanceof HDKey) {
+      DetailedAccount.checkHdKey(account)
+    } else if (account instanceof OutputDescriptor) {
+      DetailedAccount.checkOutputDescriptor(account)
     }
 
-    // only accept cryptoOutput if it has CryptoHDKey type instead of CryptoECKey
-    if (account instanceof CryptoOutput) {
-      // Check if it has hdkey
-      if (!(account.getHDKey() instanceof CryptoHDKey)) {
-        throw new Error('account must be instance of CryptoHDKey or CryptoOutput cointaining CryptoHDKey');
-      }
-      // Check if HDkey has origin and a valid path
-      CryptoDetailedAccount.checkHdKey(account.getHDKey());
-    } else {
-      // Check if HDkey has origin and a valid path
-      CryptoDetailedAccount.checkHdKey(account);
-    }
+    throw new Error('Account must be instance of HDKey or OutputDescriptor')
   }
 
-  static checkHdKey(hdKey: CryptoHDKey) {
-    // Check if hdkey has origin and a valid path
-    if (!hdKey.getOrigin() || !hdKey.getOrigin().getPath()) {
-      throw new Error("HdKey must have origin and path information eg: m/44'/60'/0'/0/0");
+  static checkOutputDescriptor(account: OutputDescriptor) {
+    // For the output descriptor, it must have only 1 key and it must be HDKey
+    const keys = account.data.keys
+
+    if (!keys || keys.length !== 1) {
+      throw new Error('Output descriptor must have only 1 key')
     }
+
+    const key = keys[0]
+    if (!(key instanceof HDKey)) {
+      throw new Error('Output descriptor key must be instance of HDKey')
+    }
+
+    // Check HDKey properties
+    DetailedAccount.checkHdKey(key)
   }
 
-  public getAccount = () => this.account;
-  public getCryptoHDKey = () => { return this.account instanceof CryptoHDKey ? this.account : undefined; };
-  public getCryptoOutput = () => { return this.account instanceof CryptoOutput ? this.account : undefined; };
-  public getTokenIds = () => {
-    if (!this.tokenIds) return undefined;
+  static checkHdKey(hdKey: HDKey) {
+    const origin = hdKey.getOrigin()
 
-    // Always return string representation of the token ids
-    return this.tokenIds.map((tokenId) => {
-      if (tokenId instanceof HexString) {
-        // Shall we really add 0x to start?
-        return '0x' + tokenId.getData();
+    if (origin == undefined) {
+      throw new Error('HDKey must have origin')
+    }
+
+    // Now for detailed account we must have only 1 path
+    // and it should only contain simple path components
+    // Eg: m/44'/60'/0
+    // The other path components are not allowed
+    // Eg: m/44/*/1-5
+    origin.getComponents().forEach(path => {
+      if (!path.isIndexComponent()) {
+        throw new Error('Detailed account path can only contain index components')
       }
-      return tokenId;
-    });
-  };
-
-  public toDataItem = () => {
-    const map: DataItemMap = {};
-    // Set account based on its type CryptoHDkey or CryptoOutput
-    let dataItemAccount: DataItem;
-
-    // Convert to data item
-    dataItemAccount = this.account.toDataItem();
-
-    const accountTag = this.account.getRegistryType().getTag();
-    // There is a bug in crypto output, it doesnt have top level datatem
-    // Add one layer of data item if its crypto output and contains tag
-    if (accountTag == RegistryTypes.CRYPTO_OUTPUT.getTag() && dataItemAccount.getTag()) {
-      dataItemAccount = new DataItem(dataItemAccount);
-    }
-
-    // Now set the tag
-    dataItemAccount.setTag(this.account.getRegistryType().getTag());
-
-    map[Keys.account] = dataItemAccount;
-
-    // Return TokenIds based on their type
-    if (this.tokenIds) {
-      map[Keys.tokenIds] = this.tokenIds.map((tokenId) => {
-        if (tokenId instanceof HexString) return tokenId.toDataItem();
-        return tokenId;
-      });
-    }
-
-    return new DataItem(map);
-  };
-
-  public static fromDataItem = (dataItem: DataItem) => {
-    const map = dataItem.getData();
-
-    let account: CryptoHDKey | CryptoOutput;
-    let tokenIds: token_id[] | undefined = undefined;
-
-    const accountDataItem = map[Keys.account] as DataItem;
-    const accountTag = accountDataItem.getTag();
-
-    // Cast to correct type based on tag
-    if (accountTag == RegistryTypes.CRYPTO_HDKEY.getTag()) {
-      account = CryptoHDKey.fromDataItem(accountDataItem);
-    } else if (accountTag == RegistryTypes.CRYPTO_OUTPUT.getTag()) {
-      // Because of the bug, we need to remove the top level data item
-      // First we will try as it should be, then with bug fix
-      try {
-        account = CryptoOutput.fromDataItem(accountDataItem);
-      } catch (error) {
-        account = CryptoOutput.fromDataItem(accountDataItem.getData());
-      }
-    } else {
-      throw new Error(`Invalid tag for account: ${accountTag}`);
-    }
-
-    // If token-ids are provided
-    if (map[Keys.tokenIds]) {
-      // Now check the type of token-ids
-      const tokenIdsRead = map[Keys.tokenIds] as (string | DataItem)[];
-
-      // Parse every element in the array
-      tokenIds = tokenIdsRead.map((tokenId) => {
-        // Check if string
-        if (typeof tokenId == 'string') return tokenId;
-        // Check if hex string
-        else if (tokenId instanceof DataItem) {
-          return HexString.fromDataItem(tokenId);
-        } else throw new Error(`Invalid type for token-id: ${typeof tokenId}`);
-      });
-    }
-
-    return new CryptoDetailedAccount(account, tokenIds);
-  };
-
-  public static fromCBOR = (_cborPayload: Buffer) => {
-    const dataItem = decodeToDataItem(_cborPayload);
-    return CryptoDetailedAccount.fromDataItem(dataItem);
-  };
+    })
+  }
 }
